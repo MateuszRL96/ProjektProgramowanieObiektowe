3775e4a2660514257838f79a32bb6936d491e2a4
MateuszRL96 <mati.kulec12@gmail.com>
2022-12-16 17:12:53 +0100

    dodanie delete uzytownika/cos niue dziala

---
+++ src/script.js
@@ -0,0 +1,27 @@
+var selectedRow = null;
+
+//show alerts
+
+function showAlert(message, className)
+{
+    const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container");
+    const main = document.querySelector(".main");
+    container.insertBefore(div, main);
+
+    setTimeout(() => document.querySelector(".alert").remove(), 3000);
+
+}
+
+document.querySelector("#student-list").addEventListener("click", (e) => 
+{
+    target = e.target;
+    if(target.classList.contains("delete"))
+    {
+        target.parentElement.parentElement.remove();
+        showAlert("Student Data Deleted", "danger");
+    }
+});
